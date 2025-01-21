1e4e3d68be05bbf8bd1a1d50b9b2b5ab50d4e181
gafur-shaikhy <gafur13032007@gmail.com>
2025-01-21 14:25:10 +0500

    update 3

---
+++ laboratories/lab_02/operators_examples.py
@@ -0,0 +1,26 @@
+# 1
+print(10 + 5)
+# ans : 15
+
+# 2
+print(5 | 3) # ans : 7 (0111)
+print(5 ^ 3) # asn : 6 (0110)
+print(5 >> 3) # ans : 0 (0000)
+print(5 << 3) # ans : 40 (101000)
+# 5 = 0101
+# 3 = 0011
+
+
+# 3 
+print((6 + 3) - (6 + 3))
+# ans : 0
+
+# 4
+print(100 + 5 * 3)
+# ans : 115
+
+# 5
+print(5 + 4 - 7 + 3)
+# ans : 5
+
+# operators_examples.py
---
+++ laboratories/lab_02/operators_exercise.py
@@ -0,0 +1,30 @@
+# 1
+x = 5
+x += 3
+print(x)
+# ans : 8
+
+# 2
+print(10 * 5)
+# ans : 50
+
+# 3
+print (10 // 2) # ans : 5
+print (10 / 2) # ans : 5.0
+
+
+# 4
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+# ans : Yes, apple is a fruit!
+
+# 5
+if 5 != 10 :
+  print("5 and 10 is not equal")
+
+#6
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+
+ # operators_exercise.py 
