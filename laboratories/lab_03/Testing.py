45a145010a3797e371c2c5b720a46c318ffeab81
gafur-shaikhy <gafur13032007@gmail.com>
2025-01-27 09:37:25 +0500

    just examples from lecture

---
+++ laboratories/lab_03/Testing.py
@@ -0,0 +1,15 @@
+# def tst():
+#     a = input()
+#     b = input()
+# tst()
+
+lang = input()
+def greet(lang):
+    if lang == "es":
+        print("Hola")
+    elif lang == "kz":
+        print("Salem")
+    else : print("Hello")
+
+greet(lang)    
+
